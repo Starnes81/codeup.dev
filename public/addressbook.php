2c0e9d8a9ce91c9d36171eecfcfd22d63d9ecbe8
Starnes81 <starnes81@gmail.com>
2014-02-26 13:09:46 -0600

    cdv address book
    user input transferring to array. Working on echoing HTML input to
    display on screen

--- public/addressbook.php
+++ public/addressbook.php
@@ -1,61 +1,60 @@
 <?php
 
-$address_book = [[]];
+$address_book = [
+	['The White House', '1600 Pennsylvania Avenue NW', 'Washington', 'DC', '20500'],
+    ['Marvel Comics', 'P.O. Box 1527', 'Long Island City', 'NY', '11101'],
+    ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901']
+];
 
-$filename = 'address_book.csv';
+$filename = ('address_book.csv');
 function open_csv($filename){
-$handle = fopen($filename, 'c+');
-foreach ($address_book as $fields) {
-	fputcsv($handle, $fields);
+	if(filesize($filename) == 0) {
+			return array();
+		}
+		$handle = fopen($filename, 'c+');
+		foreach ($address_book as $fields) {
+			fputcsv($handle, $fields);
+			}
+		fclose($handle);
 	}
-fclose($handle);
+
+
+	function save_to_file($filename, $address_book) {
+	$fields = implode("\n", $address_book);
+	$handle = fopen($filename, 'w');
+	fputcsv($handle, $fields);
+	fclose($handle);
 }
 
-$address_book = open_csv($filename)
+
+$address_book = open_csv($filename);
+
+if (!empty($_POST["item1"])){
+	$item = $_POST["item1"];
+	array_push($address_book, $fields);
+	save_to_file($filename, $fields);
+}
+
+var_dump($_POST);
 
 ?>
 
 <!DOCTYPE html>
+
 <html>
 <head>
 	<title>Addressbook</title>
 </head>
 <body>
 
-	<ul>
-	<? foreach ($address_book as $key => $item) {
-		$newTodo = $key + 1; ?>
-		<?= "<li>" . htmlspecialchars(strip_tags($item)) . " <a href='?remove=$key'>Remove Item</a></li>";
-	} ?>
- 	
-	</ul>
+	<h2>Address Book</h2>
 
-	<form method="$_POST">
-		<p>
-		<label>Enter Name: </label>
-		<input type="text" name="Name" id="item1" placeholder="Enter Name">
-		</p>
-		<p>
-		<label>Enter Address: </label>
-		<input type="text" name="Adrress" id="item2" placeholder="Enter Address">
-		</p>
-		<p>
-		<label>Enter City: </label>
-		<input type="text" name="City" id="item3" placeholder="Enter City">
-		</p>
-		<p>
-		<label>Enter State: </label>
-		<input type="text" name="State" id="item4" placeholder="Enter State">
-		</p>
-		<p>
-		<label>Enter Zip: </label>
-		<input type="text" name="State" id="item5" placeholder="Enter Zip">
-		</p>
-		<p>
-		<input type="submit" value="add" >
-	</form>
 	<table>
 		<tr>
+	<? foreach ($address_book as $key => $field) {
+		$newTodo = $key + 1; ?>
+		<?= "<td>" . strip_tags($field) . "</td>";
+	} ?>
 			<td></td>
 			<td></td>
 			<td></td>
@@ -63,6 +62,32 @@ $address_book = open_csv($filename)
 			<td></td>
 		</tr>
 	</table>
+	
+
+	<form method="POST">
+		<p>
+			<label>Enter Name: </label>
+			<input type="text" name="Name" id="item1" placeholder="Enter Name">
+		</p>
+		<p>
+			<label>Enter Address: </label>
+			<input type="text" name="Adrress" id="item2" placeholder="Enter Address">
+		</p>
+		<p>
+			<label>Enter City: </label>
+			<input type="text" name="City" id="item3" placeholder="Enter City">
+		</p>
+		<p>
+			<label>Enter State: </label>
+			<input type="text" name="State" id="item4" placeholder="Enter State">
+		</p>
+		<p>
+			<label>Enter Zip: </label>
+			<input type="text" name="State" id="item5" placeholder="Enter Zip">
+		</p>
+		<p>
+			<input type="submit" value="add" >
+	</form>
 
 </body>
 </html>

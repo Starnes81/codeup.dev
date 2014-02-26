fe17d44282e9d83621f492e8287727687cbaa2c5
Starnes81 <starnes81@gmail.com>
2014-02-26 11:31:26 -0600

    csv addressbook
    created form in php/html, continuing to php code

--- public/addressbook.php
+++ public/addressbook.php
@@ -2,16 +2,18 @@
 
 $address_book = [[]];
 
-$handle = fopen('address_book.csv', 'w');
 
+function open_csv(){
+$handle = fopen('address_book.csv', 'w');
 foreach ($address_book as $fields) {
-	fputcsv(($handle, $fields);
+	fputcsv($handle, $fields);
+	}
+fclose($handle);
 }
 
-fclose($handle);
+
+
 ?>
-
-
 
 <!DOCTYPE html>
 <html>
@@ -19,13 +21,37 @@ fclose($handle);
 	<title>Addressbook</title>
 </head>
 <body>
+	<form method="$_POST">
+		<p>
+		<label>Enter Name: </label>
+		<input type="text" name="Name" id="item1">
+		</p>
+		<p>
+		<label>Enter Address: </label>
+		<input type="text" name="Adrress" id="item2">
+		</p>
+		<p>
+		<label>Enter City: </label>
+		<input type="text" name="City" id="item3">
+		</p>
+		<p>
+		<label>Enter State: </label>
+		<input type="text" name="State" id="item4">
+		</p>
+		<p>
+		<label>Enter Zip: </label>
+		<input type="text" name="State" id="item5">
+		</p>
+		<p>
+		<input type="submit" value="add" >
+	</form>
 	<table>
 		<tr>
-			<td>cell</td>
-			<td>cell</td>
-			<td>cell</td>
-			<td>cell</td>
-			<td>cell</td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
 		</tr>
 	</table>
 

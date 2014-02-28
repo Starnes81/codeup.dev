c592b6a8c072f84bc77cd00a963a5b212bd1ce33
Starnes81 <starnes81@gmail.com>
2014-02-28 15:54:59 -0600

    addressbook
    deugged with Filestore extension

--- public/filestore.php
+++ public/filestore.php
@@ -2,18 +2,21 @@
 
 class Filestore {
 
-    public $filename = '';
+    public $filename = 'todo.txt';
+
+    public $items = array();
 
     function __construct($filename = '') {
-    	if(!empty($filename))
+    	if(!empty($filename)){
         // Sets $this->filename
-        $this->filename = $filename;
+        		$this->filename = $filename;
+    		}
     }
 
     /**
      * Returns array of lines in $this->filename
      */
-    function read_lines() {
+    public function read_lines() {
     	if(filesize($this->filename) == 0) {
 		return array();
 			}
@@ -24,20 +27,30 @@ class Filestore {
 			return $items;
     }
 
-    /**
-     * Writes each element in $array to a new line in $this->filename
-     */
-    function write_lines($array) {
+    public function write_lines($items) {
+    	var_dump($items);
     	$string = implode("\n", $items);
 		$handle = fopen($this->filename, 'w');
 		fwrite($handle, $string);
 		fclose($handle);
     }
 
+     /**
+     * Writes each element in $array to a new line in $this->filename
+     */
+
+	public function get_list() {
+		if (filesize($this->filename) > 0) {
+			return $this->read_lines($this->filename);
+		} else {
+			return array();
+		}
+	}
+
     /**
      * Reads contents of csv $this->filename, returns an array
      */
-    function read_csv() {
+    public function read_csv() {
 		$contents = [];
 		$handle = fopen($this->filename, "r");
 		while (($data = fgetcsv($handle)) !== FALSE) {
@@ -50,7 +63,7 @@ class Filestore {
     /**
      * Writes contents of $array to csv $this->filename
      */
-    function write_csv($address_book) {
+    public function write_csv($address_book) {
 			$handle = fopen($this->filename, 'w');
 			foreach ($address_book as $row) {
 				fputcsv($handle, $row);
--- public/todo-list.php
+++ public/todo-list.php
@@ -4,13 +4,12 @@
 
 require_once('filestore.php');
 
-$todo = new filestore ();
+$todo = new Filestore ();
 
 
 $items = $todo->read_lines();
 
- 
-$todo->write_lines();
+
 
 // function open_file ($filename){
 // 	if(filesize($filename) == 0) {
@@ -35,22 +34,27 @@ $todo->write_lines();
 
 // $items = open_file($this->filename);	
 
-// //load file
+//load file
 if (!empty($_POST["newitem"])){
 	$item = $_POST["newitem"];
 	array_push($items, $item);
-	save_to_file($this->filename, $items);
+	$todo->write_lines($items);
 }
+
+
 
 //remove
 if (isset($_GET['remove'])){
 	unset($items[$_GET['remove']]);
-	save_to_file($this->filename, $items);
+	$todo->write_lines($items);
 	header("Location: todo-list.php");
 	exit;
 }
 
+
 if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0){
+	var_dump($_FILES);
+	var_dump($_POST);
 	if ($_FILES['file1']['type'] != 'text/plain') {
 		$errorMsg = 'Invalid Filw type';
 		echo $errorMsg;
@@ -59,24 +63,20 @@ if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0){
 		$new_file = basename($_FILES['file1']['name']);
 		$saved_filename = $upload_dir . $new_file;
 		move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
-	    $addFile = $this->read_file($_FILES['file1']['name']);
-
+		$addFile = new Filestore($saved_filename);
+		$newitems = $addFile->read_lines();
+	  
 	    if (isset($_POST['over1']) && $_POST['over1'] == TRUE){
-	    	$items = $addFile;
+	    	$items = $newitems;
 	    }else{
-			foreach ($addFile as $key => $item) {
-	        	array_push($items, $addFile[$key]);
+			foreach ($newitems as $key => $item) {
+	        	array_push($items, $item);
 	    	}
 		} 
-    $this->write_lines($this->filename, $items);    
+    $todo->write_lines($items);    
     }
 }
     
-
-var_dump($_POST);
-var_dump(isset($_POST['over1']));
-
-
 ?>
 
 <!DOCTYPE html>

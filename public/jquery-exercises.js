a327f2cb2084cbf06dc743ebafee49687a806648
Starnes81 <starnes81@gmail.com>
2014-03-19 11:17:19 -0500

    JQuery Exercises
    cycles through exercise steps and got expected results.

---
+++ public/jquery-exercises.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>jQuery Study</title>
+</head>
+<body>
+	<script src="/js/jquery.js"></script>
+	<script type="text/javascript">
+
+	
+
+	</script>
+
+	<h1>jQuery Study</h1>
+
+	<div>
+		<p>Using jQuery selectors to modify html code. We will be accessing ID and CLASS the following:</p>
+	</div>
+
+	<div>
+		<ul>
+			<li>h1</li>
+			<li>div</li>
+			<li>p</li>
+			<li>ul</li>
+			<li>li</li>
+		</ul>
+	</div>
+
+
+
+</body>
+</html>
